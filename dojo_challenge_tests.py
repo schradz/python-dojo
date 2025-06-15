0fa5bbd5eea1e73650535c28d68b925716bf4831
Zach Schrader <zacharyschrader@proton.me>
2025-06-15 17:45:36 -0500

    wrote test cases for different user inputs

---
+++ dojo_challenge_tests.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+
+class TestDojoChallenges(unittest.TestCase):
+    '''Tests for dojo_challenges.py'''
+    @patch('builtins.input', return_value="easy")
+    def test_get_user_difficulty_easy(self, mock_input):
+        '''Does user input for easy'''
+        user_difficulty = get_user_difficulty()
+        self.assertEqual(user_difficulty, "easy")
+
+    
+    @patch('builtins.input', return_value="medium")
+    def test_get_user_difficulty_medium(self, mock_input):
+        '''Does user input for medium work'''
+        user_difficulty = get_user_difficulty()
+        self.assertEqual(user_difficulty, "medium")
+
+
+    @patch('builtins.input', return_value="hard")
+    def test_get_user_difficulty_hard(self, mock_input):
+        '''Does user input for hard work'''
+        user_difficulty = get_user_difficulty()
+        self.assertEqual(user_difficulty, "hard")
+
+    
+    patch('builtins.input', return_value="eay")
+    def test_get_user_difficulty_typo(self, mock_input):
+        '''Does user input with typo report error'''
+        user_difficulty = get_user_difficulty()
+        self.assertEqual(user_difficulty, "error")
+
+        
+if __name__ == "__main__":
+    unittest.main()
