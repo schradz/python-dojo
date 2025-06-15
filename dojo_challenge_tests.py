fb709bcd66dbcf056a24c048b07e74755e7cfdc2
Zach Schrader <zacharyschrader@proton.me>
2025-06-15 18:22:19 -0500

    silence outputs from function prompts

--- dojo_challenge_tests.py
+++ dojo_challenge_tests.py
@@ -1,32 +1,42 @@
 import unittest
 from unittest.mock import patch
+import io
 from dojo_challenge import *
+
+def suppress_print(self):
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
 
 class TestDojoChallenges(unittest.TestCase):
     '''Tests for dojo_challenges.py'''
     @patch('builtins.input', return_value="easy")
-    def test_get_user_difficulty_easy(self, mock_input):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_get_user_difficulty_easy(self, mock_stdout, mock_input):
         '''Does user input for easy'''
         user_difficulty = get_user_difficulty()
         self.assertEqual(user_difficulty, "easy")
 
     
     @patch('builtins.input', return_value="medium")
-    def test_get_user_difficulty_medium(self, mock_input):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_get_user_difficulty_medium(self, mock_stdout, mock_input):
         '''Does user input for medium work'''
         user_difficulty = get_user_difficulty()
         self.assertEqual(user_difficulty, "medium")
 
 
     @patch('builtins.input', return_value="hard")
-    def test_get_user_difficulty_hard(self, mock_input):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_get_user_difficulty_hard(self, mock_stdout, mock_input):
         '''Does user input for hard work'''
         user_difficulty = get_user_difficulty()
         self.assertEqual(user_difficulty, "hard")
 
     
     @patch('builtins.input', return_value="eay")
-    def test_get_user_difficulty_typo(self, mock_input):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_get_user_difficulty_typo(self, mock_stdout, mock_input):
         '''Does user input with typo report error'''
         user_difficulty = get_user_difficulty()
         self.assertEqual(user_difficulty, "error")
